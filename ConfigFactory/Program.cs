a1538278360570da29e7750647d2f55ca69ed277
Yves Schubert <yves.schubert@posteo.de>
2017-10-22 12:06:14 +0200

    Creates Project

---
+++ ConfigFactory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConfigFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
